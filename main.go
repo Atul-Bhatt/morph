d88b65dbda3c2ea920ec63e7c14a64f9a9cce444
Atul Bhatt <atulbhatt61@gmail.com>
2025-08-15 18:43:59 +0530

    multi page

--- main.go
+++ main.go
@@ -11,19 +11,21 @@ import (
 )
 
 func main() {
-	var text string
-
 	client := gosseract.NewClient()
 	defer client.Close()
 
-	doc, err := fitz.New("sample_text.pdf")
+	doc, err := fitz.New("metamorphosis.pdf")
 	if err != nil {
 		panic(err)
 	}
 	defer doc.Close()
 
+	ActivateTheme("azure light")
+	t := Text(Font("helvetica", 10), Padx("2m"), Pady("2m"))
+
 	// Extract pages as images and pass to tesseract
 	for n := 0; n < doc.NumPage(); n++ {
+		fmt.Println("Processing page: ", n)
 		img, err := doc.Image(n)
 		if err != nil {
 			panic(err)
@@ -34,17 +36,14 @@ func main() {
 		b := buf.Bytes()
 
 		client.SetImageFromBytes(b)
-		text, err = client.Text()
+		text, err := client.Text()
 		if err != nil {
 			fmt.Println(err)
 		}
-	}
+		Grid(t, Sticky("news"), Pady("5m"))
+		t.InsertML(text + "<br><br><br><br>")
 
-	// pass the text to UI
-	t := Text(Font("helvetica", 10), Padx("2m"), Pady("2m"))
-	t.InsertML(text)
-	ActivateTheme("azure light")
-	Grid(t, Sticky("news"), Pady("2m"))
+	}
 	Grid(TExit(), Padx("1m"), Pady("2m"), Ipadx("1m"), Ipady("1m"))
 	App.Center().Wait()
 }
