4aed8469d2cc97d9283743401604adbcff82db1c
Atul Bhatt <atulbhatt61@gmail.com>
2025-08-20 09:59:54 +0530

    OCR processing in goroutines

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/png"
 	"bytes"
 	"strings"
@@ -14,10 +15,18 @@ import (
 	"github.com/go-pdf/fpdf"
 )
 
+type SyncText struct {
+	mu   sync.Mutex
+	txt *TextWidget
+}
+
 func main() {
 	now := time.Now()
-	client := gosseract.NewClient()
-	defer client.Close()
+
+	var t SyncText
+	t.mu.Lock()
+	t.txt = Text(Font("helvetica", 10), Padx("2m"), Pady("2m"))
+	t.mu.Unlock()
 
 	doc, err := fitz.New("Metamorphosis.pdf")
 	if err != nil {
@@ -28,31 +37,32 @@ func main() {
 	ActivateTheme("azure light")
 	out := Label(Height(2), Anchor("e"), Txt("Morph PDF Editor"))
 	Grid(out, Columnspan(1), Sticky("e"))
-	t := Text(Font("helvetica", 10), Padx("2m"), Pady("2m"))
 
 	var wg sync.WaitGroup
 	// Extract pages as images and pass to tesseract
 	for n := 0; n < doc.NumPage(); n++ {
 		wg.Add(1)
-		go ImageToText(doc, t, client, n, &wg)
+		img, err := doc.Image(n)
+		if err != nil {
+			fmt.Println("Error processing image")
+		}
+		go ImageToText(img, t, n, &wg)
 	}
 	wg.Wait()
-	Grid(t, Padx("1m"), Pady("2m"), Ipadx("1m"), Ipady("1m"))
-	Grid(TButton(Txt("Save PDF"), Command(func() { SavePDF(t.Get("1.0", "end-1c")) })))
+
+	Grid(t.txt, Padx("1m"), Pady("2m"), Ipadx("1m"), Ipady("1m"))
+	Grid(TButton(Txt("Save PDF"), Command(func() { SavePDF(t.txt.Get("1.0", "end-1c")) })))
 	Grid(TExit(), Padx("1m"), Pady("2m"), Ipadx("1m"), Ipady("1m"))
-
 	fmt.Println("Time taken: ", now.Sub(time.Now()))
-
 	App.Center().Wait()
 }
 
-func ImageToText(doc *fitz.Document, t *TextWidget, client *gosseract.Client, n int, wg *sync.WaitGroup) {
+func ImageToText(img *image.RGBA, t SyncText, n int, wg *sync.WaitGroup) {
+	client := gosseract.NewClient()
+	defer client.Close()
 	defer wg.Done()
+
 	fmt.Println("Processing page: ", n)
-	img, err := doc.Image(n)
-	if err != nil {
-		panic(err)
-	}
 
 	var buf bytes.Buffer
 	png.Encode(&buf, img)
@@ -63,7 +73,10 @@ func ImageToText(doc *fitz.Document, t *TextWidget, client *gosseract.Client, n 
 	if err != nil {
 		fmt.Println(err)
 	}
-	t.InsertML(text + "<br>" + string(n) + "<br>")
+	t.mu.Lock()
+	t.txt.InsertML(text + "<br>" + string(n) + "<br>")
+	t.mu.Unlock()
+	fmt.Println("Processing complete: ", n)
 }
 
 func SavePDF(text []string) {
